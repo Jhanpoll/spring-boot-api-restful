f42fa346a68e4c2268842dedbde30b8c020068d2
cherito <cheroarge96@gmail.com>
2021-07-21 14:24:50 -0500

    uso de properties y valores

--- src/main/java/com/fundamentosspringboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosspringboot/fundamentos/FundamentosApplication.java
@@ -2,6 +2,7 @@ package com.fundamentosspringboot.fundamentos;
 
 import com.fundamentosspringboot.fundamentos.bean.MyBean;
 import com.fundamentosspringboot.fundamentos.bean.MyBeanWithDependeny;
+import com.fundamentosspringboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosspringboot.fundamentos.component.Component;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -14,10 +15,13 @@ public class FundamentosApplication implements CommandLineRunner {
 	private Component component;
 	private MyBean myBean;
 	private MyBeanWithDependeny myBeanWithDependeny;
-	public FundamentosApplication(@Qualifier("componentTwo") Component component,MyBean myBean, MyBeanWithDependeny myBeanWithDependeny) {
+	private MyBeanWithProperties myBeanWithProperties;
+
+	public FundamentosApplication(@Qualifier("componentTwo") Component component,MyBean myBean, MyBeanWithDependeny myBeanWithDependeny, MyBeanWithProperties myBeanWithProperties) {
 		this.component= component;
 		this.myBean=myBean;
 		this.myBeanWithDependeny=myBeanWithDependeny;
+		this.myBeanWithProperties=myBeanWithProperties;
 	}
 
 	public static void main(String[] args) {
@@ -29,5 +33,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		component.leer();
 		myBean.print();
 		myBeanWithDependeny.printWithDependeny();
+		System.out.println(myBeanWithProperties.function());
 	}
 }
---
+++ src/main/java/com/fundamentosspringboot/fundamentos/configuration/GeneralConfiguration.java
@@ -0,0 +1,29 @@
+package com.fundamentosspringboot.fundamentos.configuration;
+
+import com.fundamentosspringboot.fundamentos.bean.MyBeanWithProperties;
+import com.fundamentosspringboot.fundamentos.bean.MyBeanWithPropertiesImplement;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class GeneralConfiguration {
+
+    // configuraciones y dependencias
+    @Value("${value.name}")
+    private String name;
+
+    @Value("${value.apellido}")
+    private String apellido;
+
+    @Value("${value.random}")
+    private String random;
+
+    @Bean
+    public MyBeanWithProperties function(){
+        return new MyBeanWithPropertiesImplement(name, apellido);
+    }
+
+
+}
